090aa7a73848ebfa2febf3047bf6bac53525216e
itCanelita <abelastacio86@gmail.com>
2023-10-11 09:30:00 -0400

    Login - Registro funcional

---
+++ Api/iniciarsesion.js
@@ -0,0 +1,35 @@
+const btnSubmit = document.getElementById("btn-submit"); 
+const cajaUsuario = document.querySelector("#usuario")
+const cajaClave = document.querySelector("#clave")
+
+
+
+btnSubmit.addEventListener("click", () => {
+    let Usuario = cajaUsuario.value
+    let Clave = cajaClave.value
+    
+    const usuario = {
+      NombreUsuario: Usuario,
+      Contraseña: Clave
+    };
+    fetch("https://localhost:7031/Usuario/login", {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json"
+    },
+    body: JSON.stringify(usuario)
+    })
+    .then(response => {
+        if (!response.ok) {
+        throw new Error("No se pudo autenticar");
+        }
+        return response.text(); // Suponiendo que el servidor devuelve un mensaje de éxito
+    })
+    .then(data => {
+        console.log("Autenticación exitosa:", data);
+        // Aquí puedes redirigir al usuario o realizar otras acciones
+    })
+    .catch(error => {
+        console.error("Error de autenticación:", error);
+    });
+});
